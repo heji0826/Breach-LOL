9a20ea5ec2c71c824c61a129d72467576ab0a08f
hyj765 <hyj765@naver.com>
2024-11-23 17:33:12 +0900

    web auto scaling & web loadbalancer

--- web/web_service_infra.tf
+++ web/web_service_infra.tf
@@ -33,13 +33,13 @@ resource "aws_internet_gateway" "web_igw" {
  subnet 선언 
 */
 resource "aws_subnet" "public_elb_subnet" {
-  count = 2
-  vpc_id = aws_vpc.web_vpc.id
-  cidr_block = element(["192.168.21.0/24", "192.168.22.0/24"], count.index)
+  count             = 2
+  vpc_id            = aws_vpc.web_vpc.id
+  cidr_block        = element(["192.168.21.0/24", "192.168.22.0/24"], count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
-
+  
   tags = {
-    "Name" = "elb_public_subnet${count.index + 1 }"
+    "Name" = "elb_public_subnet${count.index + 1}"
   }
 
 }
@@ -114,13 +114,7 @@ resource "aws_network_acl" "web_nat_gateway_nacl" {
     cidr_block = "0.0.0.0/0"
     action     = "allow"
   }
-  
-  ingress {
-    rule_no = 100
-    protocol = "-1"
-    #룰 추가해야함
-    
-  }
+
   egress {
     rule_no    = 100
     protocol   = "-1"
@@ -160,7 +154,7 @@ resource "aws_route_table" "internet_route_table" {
 
 resource "aws_route_table" "private_nat_route_table" {
   vpc_id = aws_vpc.web_vpc.id
-  
+
   route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.web_nat_gateway.id
@@ -172,8 +166,8 @@ resource "aws_route_table" "private_nat_route_table" {
 }
 
 resource "aws_route_table_association" "elb_route_associate" {
-  count = length(aws_subnet.public_elb_subnet[*].id)
-  subnet_id = aws_subnet.public_elb_subnet[count.index].id
+  count          = length(aws_subnet.public_elb_subnet[*].id)
+  subnet_id      = aws_subnet.public_elb_subnet[count.index].id
   route_table_id = aws_route_table.internet_route_table.id
 }
 
@@ -243,16 +237,16 @@ resource "aws_security_group" "http_open" {
   vpc_id = aws_vpc.web_vpc.id
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -260,17 +254,17 @@ resource "aws_security_group" "http_open" {
 
 resource "aws_security_group" "https_open" {
   vpc_id = aws_vpc.web_vpc.id
-  
+
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -305,12 +299,12 @@ resource "aws_nat_gateway" "web_nat_gateway" {
 }
 
 resource "aws_instance" "bastion_host" {
-  ami           = "ami-040c33c6a51fd5d96"
-  instance_type = "t3.small"
-  subnet_id     = aws_subnet.public_bastion_host_subnet.id
-  key_name = "public-ec2-key"
+  ami                         = "ami-040c33c6a51fd5d96"
+  instance_type               = "t3.small"
+  subnet_id                   = aws_subnet.public_bastion_host_subnet.id
+  key_name                    = "public-ec2-key"
   associate_public_ip_address = true
-  security_groups = [aws_security_group.icmpopen.id,aws_security_group.public_bastion_host_sg.id]
+  security_groups             = [aws_security_group.icmpopen.id, aws_security_group.public_bastion_host_sg.id]
 
   tags = {
     "Name" = "web_service_bastion_host_instance"
@@ -319,12 +313,12 @@ resource "aws_instance" "bastion_host" {
 }
 
 resource "aws_instance" "aws_web_servers" {
-  count         = (length(aws_subnet.private_web_subnet))
-  ami           = "ami-040c33c6a51fd5d96"
-  instance_type = "t3.small"
-  subnet_id     = aws_subnet.private_web_subnet[count.index].id
-  key_name = "public-ec2-key"
-  security_groups = [ aws_security_group.icmpopen.id , aws_security_group.public_bastion_host_sg.id , aws_security_group.http_open.id, aws_security_group.https_open.id]
+  count           = (length(aws_subnet.private_web_subnet))
+  ami             = "ami-040c33c6a51fd5d96"
+  instance_type   = "t3.small"
+  subnet_id       = aws_subnet.private_web_subnet[count.index].id
+  key_name        = "public-ec2-key"
+  security_groups = [aws_security_group.icmpopen.id, aws_security_group.public_bastion_host_sg.id, aws_security_group.http_open.id, aws_security_group.https_open.id]
   tags = {
     "Name" = "web_server_instance_${count.index}"
   }
@@ -344,7 +338,7 @@ resource "aws_db_subnet_group" "web_db_subnet_group" {
 }
 
 resource "aws_db_subnet_group" "web_db_master_subnet_group" {
-  name = "web_master_subnet_group"
+  name       = "web_master_subnet_group"
   subnet_ids = aws_subnet.private_db_subnet[*].id
   tags = {
     "Name" = "web_db_subnet_group"
@@ -357,18 +351,18 @@ resource "aws_db_subnet_group" "web_db_master_subnet_group" {
 */
 
 resource "aws_db_instance" "web_primary_db_server" {
-  allocated_storage = 10
-  db_name = "web_db"
-  engine = "mariadb"
-  engine_version = "10.11.9"
-  instance_class = "db.t3.micro"
-  username = "admin"
-  password = "SecurePssw0rd"
-  db_subnet_group_name = aws_db_subnet_group.web_db_master_subnet_group.id
-  publicly_accessible = false
-  backup_retention_period = 7
-  final_snapshot_identifier = "web-primary-db"
-  identifier = "web-primary-db"
+  allocated_storage         = 10
+  db_name                   = "web_db"
+  engine                    = "mariadb"
+  engine_version            = "10.11.9"
+  instance_class            = "db.t3.micro"
+  username                  = "admin"
+  password                  = "SecurePssw0rd"
+  db_subnet_group_name      = aws_db_subnet_group.web_db_master_subnet_group.id
+  publicly_accessible       = false
+  backup_retention_period   = 7
+  skip_final_snapshot = true
+  identifier                = "web-primary-db"
   tags = {
     "Name" = "web_Server_Primary_DB"
   }
@@ -376,20 +370,19 @@ resource "aws_db_instance" "web_primary_db_server" {
 
 resource "aws_db_instance" "aws_read_replica_db_server" {
 
-  count                = 2
-  engine               = "mariadb"
-  engine_version       = "10.11.9"
-  instance_class       = "db.t3.micro"
-  
-  replicate_source_db = aws_db_instance.web_primary_db_server.identifier
-  skip_final_snapshot = false
-  publicly_accessible  = false
+  count          = 2
+  engine         = "mariadb"
+  engine_version = "10.11.9"
+  instance_class = "db.t3.micro"
 
-  identifier           = "web-read-replica-db${count.index}"
+  replicate_source_db = aws_db_instance.web_primary_db_server.identifier
+  skip_final_snapshot = true
+  publicly_accessible = false
+  identifier = "web-read-replica-db${count.index}"
   tags = {
     "Name" = "webDatabase${count.index}"
   }
-  depends_on = [ aws_db_instance.web_primary_db_server ]
+  depends_on = [aws_db_instance.web_primary_db_server]
 }
 
 
@@ -404,10 +397,20 @@ resource "aws_db_instance" "aws_read_replica_db_server" {
 
 
 resource "aws_lb_target_group" "web_server_loadbalancer_tg" {
-  name     = "webServerLoadLanaberTargetGroup"
+  name     = "web-service-loadbalancer-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.web_vpc.id
+  
+  health_check {
+    protocol = "HTTP"
+    path = "/"
+    port = "80"
+    interval = 30
+    timeout = 5
+    healthy_threshold = 3
+    unhealthy_threshold = 3
+  }
 }
 
 
@@ -487,11 +490,13 @@ resource "aws_lb_listener" "http" {
 
 */
 resource "aws_s3_bucket" "waf_log_storage" {
-  bucket = "web-waf-logs-bucket"
+  bucket = "aws-waf-logs-web-service-team-logs-bucket"
+  force_destroy = true
 }
 
 resource "aws_s3_bucket" "cloudtrail_backup_storage" {
   bucket = "cloudtrail-backup-log"
+  force_destroy = true
 }
 /*
  iam 계정생성 
@@ -500,23 +505,26 @@ resource "aws_s3_bucket" "cloudtrail_backup_storage" {
 resource "aws_iam_role" "waf_log_role" {
   name = "waf-log-role"
 
-  assume_role_policy = jsondecode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "wafv2.amazonaws.com"
-        }
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "wafv2.amazonaws.com"
       }
-    ]
-  })
+    }
+  ]
+}
+POLICY
 }
 
 /*
   정책 생성
 */
+
 
 resource "aws_iam_policy" "waf_s3_log_policy" {
   name        = "WAF-S3-Log-Policy"
@@ -525,9 +533,9 @@ resource "aws_iam_policy" "waf_s3_log_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action    = "s3:PutObject",
-        Effect    = "Allow",
-        Resource  = "arn:aws:s3:::web-waf-logs-bucket/*"  # WAF가 로그를 쓸 S3 버킷
+        Action   = "s3:PutObject",
+        Effect   = "Allow",
+        Resource = "${aws_s3_bucket.waf_log_storage.arn}/*"
       }
     ]
   })
@@ -539,22 +547,10 @@ resource "aws_iam_policy" "waf_s3_log_policy" {
 
 resource "aws_iam_role_policy_attachment" "waf_log_role_policy_attachment" {
   policy_arn = aws_iam_policy.waf_s3_log_policy.arn
-  role = aws_iam_role.waf_log_role.name
+  role       = aws_iam_role.waf_log_role.name
 
-  depends_on = [ aws_iam_policy.waf_s3_log_policy, aws_iam_role.waf_log_role]
+  depends_on = [aws_iam_policy.waf_s3_log_policy , aws_iam_role.waf_log_role]
 }
-
-# 기본적으로 elb에는 standard aws_shield가 활성화되어 있음.
-# resource "aws_shield_protection" "web_alb_shield_protection" {
-#   name = "web-service-aws-shield"
-#   resource_arn = aws_lb.web_servcer_loadbalancer.arn
-
-#   tags = {
-#     "Name" = "web-elb-aws-shield"
-#   }
-#   depends_on = [ aws_lb.web_servcer_loadbalancer ]
-# }
-
 
 /*
  보안장비 instance code block
@@ -562,24 +558,24 @@ resource "aws_iam_role_policy_attachment" "waf_log_role_policy_attachment" {
 */
 
 # resource "cloudtrail" "web_service_cloudtrail" {
-  
+
 # }
 
 
 resource "aws_wafv2_web_acl" "web_acl" {
-  name ="web-acl-for-alb"
+  name  = "web-acl-for-alb"
   scope = "REGIONAL"
   default_action {
     allow {
-      
+
     }
   }
 
   visibility_config {
     cloudwatch_metrics_enabled = true
-    metric_name = "AWSManagedRulesCommonRuleSet"
-    sampled_requests_enabled = true
-  }  
+    metric_name                = "AWSManagedRulesCommonRuleSet"
+    sampled_requests_enabled   = true
+  }
 }
 
 /*
@@ -588,19 +584,60 @@ resource "aws_wafv2_web_acl" "web_acl" {
 
 resource "aws_wafv2_web_acl_logging_configuration" "waf_logging_acl_configure" {
   log_destination_configs = [aws_s3_bucket.waf_log_storage.arn]
-  resource_arn = aws_wafv2_web_acl.web_acl.arn
+  resource_arn            = aws_wafv2_web_acl.web_acl.arn
   redacted_fields {
     single_header {
       name = "user-agent"
     }
   }
+
+  depends_on = [ aws_s3_bucket.waf_log_storage ]
 }
 
 
 /*
-
+  auto scaling 설정 코드 블록
 
 */
+
+resource "aws_launch_template" "web_service_launch_config" {
+  image_id = "ami-0b793e03812de20d6"
+  instance_type = "t3.small"
+  key_name = "public-ec2-key"
+
+  network_interfaces {
+    security_groups = [aws_security_group.icmpopen.id, aws_security_group.public_bastion_host_sg.id, aws_security_group.http_open.id, aws_security_group.https_open.id]
+  }
+
+  tags ={
+    "Name" = "web-service-ec2-instance"
+  }
+}
+
+
+# /*
+#   auto scaling
+# */
+resource "aws_autoscaling_group" "web_service_autoscaling_group" {
+  name = "web-service-asg"
+  max_size = 4
+  min_size = 2
+  vpc_zone_identifier = [ for subnet in aws_subnet.private_web_subnet : subnet.id]
+  
+
+  launch_template {
+    id = aws_launch_template.web_service_launch_config.id
+    version = "$Latest"
+  }
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 60
+    }
+  }
+}
+
 
 output "aws_bastion_public_ip" {
   value = aws_instance.bastion_host.public_ip
